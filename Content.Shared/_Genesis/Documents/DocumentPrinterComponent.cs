44935873e26785728b4524ef454a8da3514011f0
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2025-12-02 13:39:28 +0300

    Delete Content.Shared/_Genesis/Documents directory

--- Content.Shared/_Genesis/Documents/DocumentPrinterComponent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Shared._Genesis.Documents;
-
-[RegisterComponent]
-public sealed partial class DocumentPrinterComponent : Component
-{
-    [DataField]
-    public string SlotName = "id";
-}
